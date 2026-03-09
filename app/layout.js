5ce0e0b565952aa4fbd530be448c493732ffa7bf
vg435294-ship-it <vg435294@gmail.com>
2026-03-09 14:59:57 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Hyper Store7",
+  description: "Cursos online com acesso rápido",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
